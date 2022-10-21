097fbf89e1f108df423bcfb3a82fccede5f92234
Chamane Barbatti <chamane@sbcglobal.net>
2022-10-21 14:49:04 -0500

    Finished Exception Exercise

--- ExceptionsExercises/Program.cs
+++ ExceptionsExercises/Program.cs
@@ -7,23 +7,57 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
-        /*
+       
+        
         static double Divide(double x, double y)
         {
-            // Write your code here!
+           if (y == 0.0)
+            {
+                throw new ArgumentOutOfRangeException("y", "You cannot divide by zero!");
+            }
+            else
+            {
+                return x / y;
+            }
         }
 
         static int CheckFileExtension(string fileName)
         {
-            // Write your code here!
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentNullException("filename" , "Student did not submit any work!");
+            }
+            else
+            {
+                if (fileName.Substring(fileName.Length - 3, 3) == ".cs")
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+
+                }
+
+            }
         }
-        */
+        
 
         static void Main(string[] args)
         {
-            // Test out your Divide() function here!
+            double a = 9.9;
+            double b = 0.0;
 
-            // Test out your CheckFileExtension() function here!
+            try
+            {
+                Divide(a, b);
+            }
+               catch (ArgumentOutOfRangeException e)
+            
+            {
+                Console.WriteLine(e.Message);
+            }
+         
 
             Dictionary<string, string> students = new Dictionary<string, string>();
             students.Add("Carl", "Program.cs");
@@ -31,7 +65,18 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
 
-
+            foreach (KeyValuePair<string, string> student in students)
+            {
+                try
+                {
+                    Console.WriteLine(student.Key);
+                    CheckFileExtension(student.Value);
+                }
+                catch(ArgumentNullException n)
+                {
+                    Console.WriteLine(n.Message);
+                }
+            }
         }
     }
 }
